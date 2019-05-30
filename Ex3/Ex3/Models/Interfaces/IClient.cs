03e17fcb7e098a4d119fe0822d7f2f93ee351f89
Naveh Marchoom <naveh94@gmail.com>
2019-05-30 16:38:47 +0300

    Added 2 interfaces: IPointSource, IClient Added class: Point

---
+++ Ex3/Ex3/Models/Interfaces/IClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3.Models.Interfaces
+{
+    interface IClient
+    {
+        string IP { get; set; }
+        int Port { get; set; }
+
+        void Connect();
+        void Disconnect();
+        string GetValue(string input);
+    }
+}
---
+++ Ex3/Ex3/Models/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ex3.Models.Interfaces
+{
+    public class Point
+    {
+        int X { get; set; }
+        int Y { get; set; }
+    }
+}
